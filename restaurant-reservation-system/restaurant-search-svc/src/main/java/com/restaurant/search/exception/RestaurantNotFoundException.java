74185d275d4d347c933b94998d8b297f87050110
Joshua1171 <59849366+joshua1171@users.noreply.github.com>
2026-05-07 00:18:45 -0600

    Add files via upload

--- restaurant-reservation-system/restaurant-search-svc/src/main/java/com/restaurant/search/exception/RestaurantNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.restaurant.search.exception;
-
-public class RestaurantNotFoundException extends RuntimeException {
-    public RestaurantNotFoundException(final String message) {
-        super(message);
-    }
-}
